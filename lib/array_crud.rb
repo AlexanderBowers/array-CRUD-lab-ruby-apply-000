025d6033715298942f4144c121121a4463eefc25
Alexander Bowers <7319abowers@gmail.com>
2020-09-29 22:25:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-array2 = [arrays!, arrays!, arrays!, arrays!]
+array2 = [wow!, wow!, wow!, wow!]
 end
 
 def add_element_to_end_of_array(array, element)
